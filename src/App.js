cf45af4afc00e0eddba95604337188213a15bc80
vainawebm <vainaweb@gmail.com>
2020-08-19 16:28:44 -0300

    novos arquivos criados

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import jhonny from './assets/sprite.png';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Level = styled.div`
+  position: relative;
+  width: 700px;
+  height: 350px;
+  background-color: #a35409;
+`;
+
+const Npc = styled.div`
+  position: absolute;
+  top: ${props => props.position[1]}px;
+  left: ${props => props.position[0]}px;
+  width: 35px;
+  height: 35px;
+  background: url(${jhonny});
+  background-position: ${props => props.side};
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // cada posição do personagem tem 35px de tamanho
+    // para mover o boneco para baixo: background-position: 0 0
+    // para mover o boneco para cima: background-position: 0 70px
+    // para mover o boneco para a esquerda: background-position: 0 35px
+    // para mover o boneco para a direita: background-position: 0 105px
+
+    
+    this.state = {
+      position: [0, 0],
+      side: '0 105px'
+    };
+  }
+
+  render() {
+    const { position, side } = this.state;
+
+    return (
+      <Container>
+        <Level>
+          <Npc position={position}
+               side={side}
+          ></Npc>
+        </Level>
+      </Container>
+    );
+  }
 }
 
 export default App;
